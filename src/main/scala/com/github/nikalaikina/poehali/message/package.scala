2eb1157899e475e5de2920b7d8794bd6bf0dbf81
Vera Nikalaikina <v.nikalaikina@adform.com>
2016-12-08 14:15:14 +0300

    cityNames route

--- src/main/scala/com/github/nikalaikina/poehali/message/package.scala
+++ src/main/scala/com/github/nikalaikina/poehali/message/package.scala
@@ -9,6 +9,7 @@ case class GetRoutees(trip: Trip) extends Request
 case class GetTickets(direction: CityDirection, dateFrom: LocalDate, dateTo: LocalDate, direct: Boolean, passengers: Int = 1) extends Request
 case class GetPlaces(number: Int = Integer.MAX_VALUE) extends Request
 case object GetCities extends Request
+case object GetCityNames extends Request
 
 sealed trait Response
 case class Routes(routes: List[TripRoute]) extends Response
